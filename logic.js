f9212583df7e00772818aa8439efb4267712e20a
nrg361 <81407875+nrg361@users.noreply.github.com>
2021-03-28 16:02:04 +0530

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,3 +1,7 @@
+var a = 1800;
+var times;
+var strt = false;
+
 function start() {
     var sudoku = [[]];
     for (var i = 0; i < 9; i++) {
@@ -22,6 +26,7 @@ function start() {
 
     else if (solve(sudoku, 0, 0)) { }
     else alert("No solution exists");
+
 }
 
 function initialCheck(sudoku) {
@@ -48,8 +53,30 @@ function initialCheck(sudoku) {
     return true;
 }
 
-function solve(sudoku, row, col) {
+function finalCheck(sudoku) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            if (sudoku[i][j] > 9 || sudoku[i][j] < 1) return false;
 
+            for (var k = 0; k < 9; k++) if (sudoku[i][j] == sudoku[k][j] && k != i) return false;
+
+            for (var k = 0; k < 9; k++) if (sudoku[i][j] == sudoku[i][k] && k != j) return false;
+
+            var boxrow = i - i % 3;
+            var boxcol = j - j % 3;
+            for (var k = 0; k < 3; k++) {
+                for (var l = 0; l < 3; l++) {
+                    if (sudoku[k + boxrow][l + boxcol] == sudoku[i][j] && (k + boxrow != i || l + boxcol != j))
+                        return false;
+                }
+            }
+
+        }
+    }
+    return true;
+}
+
+function solve(sudoku, row, col) {
     if (row == 9) {
         show(sudoku);
         return true;
@@ -105,3 +132,93 @@ function show(sudoku) {
     }*/
 }
 
+function game() {
+    if (strt == false) {
+        times = setInterval(time, 1000);
+        strt = true;
+        var sudoku = [[]];
+        for (var i = 0; i < 9; i++) {
+            for (var j = 0; j < 9; j++) {
+                const val = document.getElementById(String(i) + String(j)).value
+
+                sudoku[i].push(Number(val));
+
+            }
+            sudoku.push([]);
+        }
+
+        var cnt = 0;
+
+        while (cnt < 15) {
+            var a = Math.floor((Math.random()) * 10);
+            if (a == 9) a = 2;
+            var b = Math.floor((Math.random()) * 10);
+            if (b == 9) b = 7;
+            var c = Math.floor((Math.random()) * 10);
+            if (c == 0) c = 5;
+
+            if (sudoku[a][b] == 0 && valid(sudoku, a, b, c)) {
+                document.getElementById(String(a) + String(b)).value = c;
+                sudoku[a][b] = c;
+                cnt++;
+            }
+
+
+        }
+        
+    }
+    else {
+        clear();
+    }
+
+}
+
+function submit() {
+    var sudoku = [[]];
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            const val = document.getElementById(String(i) + String(j)).value
+
+            sudoku[i].push(Number(val));
+
+        }
+        sudoku.push([]);
+    }
+
+    var won = finalCheck(sudoku);
+    clearInterval(times);
+    if (won) alert("You won the game!")
+    else alert("You lose");
+
+}
+
+
+
+function time() {
+    a--;
+    var b = Math.floor(a / 60);
+    var c = a % 60;
+    if (b < 10)
+        minu.innerHTML = '0' + String(b);
+    else minu.innerHTML = String(b);
+    if (c < 10)
+        secs.innerHTML = '0' + String(c);
+    else secs.innerHTML = String(c);
+
+    if (c == 0 && b == 0) {
+        alert("Time's Up!");
+        clearInterval(times);
+    }
+
+}
+
+function clear() {
+    strt = false;
+    clearInterval(times);
+    for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++)
+            document.getElementById(String(i) + String(j)).value = 0;
+
+    a = 1801;
+    game();
+}
